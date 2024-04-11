e2c64de423fdef65ba221910221cd59922ca59fb
darkvuk <dark.vukasovic@gmail.com>
2024-04-11 14:30:41 +0200

    Add update tag API

--- recipe/tests/test_tag_api.py
+++ recipe/tests/test_tag_api.py
@@ -13,6 +13,11 @@ from recipe.serializers import TagSerializer
 TAGS_URL = reverse('recipe:tag-list')
 
 
+def detail_url(tag_id):
+    """Create and return a tag detail url."""
+    return reverse('recipe:tag-detail', args=[tag_id])
+
+
 def create_user(email='user@example.com', password='testpassword123'):
     """Create and return a new user"""
     return get_user_model().objects.create_user(email, password)
@@ -64,5 +69,18 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
         self.assertEqual(res.data[0]['id'], tag.id)
+
+    def test_update_tag(self):
+        """Test updating a tag"""
+        tag = Tag.objects.create(user=self.user, name='After Dinner')
+        
+        data = {'name': 'Dessert'}
+        url = detail_url(tag.id)
+        res = self.client.patch(url, data)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        tag.refresh_from_db()
+        self.assertEqual(tag.name, data['name'])
+
 
 
